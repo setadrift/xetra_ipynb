9e4180fee67e18a4609dd7ae82383a297b6248d2
setadrift <duncan.kg.anderson@gmail.com>
2022-06-12 10:28:02 -0400

    init tests

---
+++ xetra/common/custom_exceptions.py
@@ -0,0 +1,9 @@
+"""Custom Exceptions"""
+
+class WrongFormatException(Exception):
+    """
+    WrongFormatExceptoin class
+
+    Exception that can be raised when the format type 
+    given as a parameter is not supported
+    """
